fa4d17a89d018de1f5cda5587c59ef3aae72ee27
Frederik Jensen <kungfu15447@users.noreply.github.com>
2018-12-14 14:01:34 +0100

    Skrevet java doc til MyTunesViewController

--- MyTunes/src/mytunes/GUI/Controller/MyTunesViewController.java
+++ MyTunes/src/mytunes/GUI/Controller/MyTunesViewController.java
@@ -11,6 +11,8 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
@@ -64,8 +66,6 @@ public class MyTunesViewController implements Initializable
     private PlaylistSongModel psm;
     private PlaylistModel pm;
     private SongModel sm;
-    private SongSearcher ss;
-    private JFXPanel fxPanel;
     private MediaPlayer mediaPlayer;
     private boolean playing;
     private boolean paused;
@@ -98,6 +98,9 @@ public class MyTunesViewController implements Initializable
     @FXML
         private TableColumn<Playlist, String> cellPlaylistTitle;
 
+    /**
+     * The constructor of the MyTunesViewController
+     */
     public MyTunesViewController()
     {
         try
@@ -105,8 +108,6 @@ public class MyTunesViewController implements Initializable
             psm = new PlaylistSongModel();
             pm = new PlaylistModel();
             sm = new SongModel();
-            ss = new SongSearcher();
-            fxPanel = new JFXPanel();
         } catch (MTBllException ex)
         {
             displayError(ex);
@@ -130,41 +131,25 @@ public class MyTunesViewController implements Initializable
         tablePlaylist.setItems(pm.getAllPlaylist());
     }
 
+    /**
+     * When the button labelled "new..." over at the table view with playlist
+     * is pressed this will with then run this method. This opens up a new
+     * vew where the user then can create a new playlist. This method also calls
+     * the method initializeModel from inside the CreatePlaylistController.
+     * This will make it so the PlaylistModel this class calls will also be
+     * called in the CreatePlaylistController
+     * @param event The event that the button has
+     */
     @FXML
-    private void createPlaylist(ActionEvent event) throws IOException
+    private void createPlaylist(ActionEvent event)
     {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytunes/GUI/CreatePlaylist.fxml"));
-        Parent root = (Parent) loader.load();
-
-        CreatePlaylistController cpcontroller = loader.getController();
-        cpcontroller.initializeModel(pm);
-        Stage stage = new Stage();
-
-        Image icon = new Image(getClass().getResourceAsStream("/mytunes/GUI/newicon.png"));
-        stage.getIcons().add(icon);
-        stage.setTitle("MyTunes");
-        
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-    
-
-    @FXML
-    private void editPlaylist(ActionEvent event) throws IOException
-    {
-        Playlist playlist = tablePlaylist.getSelectionModel().getSelectedItem();
-        int index = tablePlaylist.getSelectionModel().getSelectedIndex();
-        if (playlist == null)
+        try
         {
-            displayNoPlaylistWindow();
-        } else
-        {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytunes/GUI/EditPlaylist.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytunes/GUI/CreatePlaylist.fxml"));
             Parent root = (Parent) loader.load();
-
-            EditPlaylistController epcontroller = loader.getController();
-            epcontroller.initializeModel(pm);
-            epcontroller.initializePlaylist(playlist, index);
+            
+            CreatePlaylistController cpcontroller = loader.getController();
+            cpcontroller.initializeModel(pm);
             Stage stage = new Stage();
             
             Image icon = new Image(getClass().getResourceAsStream("/mytunes/GUI/newicon.png"));
@@ -173,9 +158,65 @@ public class MyTunesViewController implements Initializable
             
             stage.setScene(new Scene(root));
             stage.show();
+        } catch (IOException ex)
+        {
+            displayError(ex);
+        }
+    }
+    
+    /**
+     * When the button labelled "edit..." is pressed over at the table view
+     * containing playlists then this method will run. This will open up a new
+     * view where the user can then edit a playlist. This method also checks if
+     * a playlist has been clicked before it opens up the new view. This method
+     * also calls the method initializeModel from the EditPlaylistController
+     * This makes it so the playlistmodel in this controller also gets called
+     * in the EditPlaylistController
+     * @param event the event when the button is pressed
+     */
+    @FXML
+    private void editPlaylist(ActionEvent event)
+    {
+        Playlist playlist = tablePlaylist.getSelectionModel().getSelectedItem();
+        int index = tablePlaylist.getSelectionModel().getSelectedIndex();
+        if (playlist == null)
+        {
+            displayNoPlaylistWindow();
+        } else
+        {
+            try
+            {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytunes/GUI/EditPlaylist.fxml"));
+                Parent root = (Parent) loader.load();
+                
+                EditPlaylistController epcontroller = loader.getController();
+                epcontroller.initializeModel(pm);
+                epcontroller.initializePlaylist(playlist, index);
+                Stage stage = new Stage();
+                
+                Image icon = new Image(getClass().getResourceAsStream("/mytunes/GUI/newicon.png"));
+                stage.getIcons().add(icon);
+                stage.setTitle("MyTunes");
+                
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException ex)
+            {
+                displayError(ex);
+            }
         }
     }
 
+    /**
+     * When the button labelled "delete" over at the tableview containing
+     * playlist then this method wil run. This method opens up a pop up window
+     * asking the user if they are sure they want to delete a playlist. If
+     * the press "yes" then this will run the deletePlaylist method from the
+     * PlaylistModel class and the deleteFromPlaylist method from the
+     * PlaylistSongModel class. this will delete the playlist and its
+     * information from the Playlist and PlaylistSong table
+     * @param event the event when the button is pressed
+     */
     @FXML
     private void deletePlaylist(ActionEvent event)
     {
@@ -218,6 +259,13 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * When the button containing the up arrow over at the tabelview containing 
+     * songs from a playlis then this method will run. This method will try
+     * to call the moveSong method from the PlaylistSongModel and then move the
+     * song one up from its current position
+     * @param event the event when the button is pressed
+     */
     @FXML
     private void moveUp(ActionEvent event)
     {
@@ -234,6 +282,11 @@ public class MyTunesViewController implements Initializable
 
     }
 
+    /**
+     * This method does the same as the moveUp method only difference is that
+     * this method will try to move the song one down from its current position
+     * @param event the event when the button is pressed
+     */
     @FXML
     private void moveDown(ActionEvent event)
     {
@@ -249,6 +302,14 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * When the button "delete" over at the tableview containing songs from
+     * playlists is pressed this method will run. When a playlist and a song
+     * from the playlist is selected this method will run the method
+     * removeSongFromPlaylist from the PlaylistSongModel class. This will then
+     * delete the song from the playlist
+     * @param event 
+     */
     @FXML
     private void deleteSongOnPlaylist(ActionEvent event)
     {
@@ -273,8 +334,18 @@ public class MyTunesViewController implements Initializable
 
     }
 
+    /**
+     * When the button labelled "new..." over at the tableview that contains
+     * all songs then this method will run. This method opens up a new view
+     * where the user can add a new song to the tableview. Before the view is
+     * open the method initializeModel in the CreateSongController will run.
+     * This method makes it so the instance of the SongModel in this class
+     * will also be called in the CreateSongController
+     * @param event the event when button is pressed
+     * @throws SQLException 
+     */
     @FXML
-    private void newSong(ActionEvent event) throws SQLException
+    private void newSong(ActionEvent event)
     {
         try
         {
@@ -296,6 +367,19 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * When the button labelled "edit..." is pressed over at the tableview
+     * that contains all songs is pressed then this method will run.
+     * When a song is pressed on the tableview then a new view will appear
+     * where the user then can edit an alreaady existing song. Beofre the view
+     * appears the two methods initializeModel and initializeSong will be
+     * called from the EditSongController class. The initializeModel method
+     * makes it so the SongModel used in this class will also be called in
+     * the EditSongController. The initializeSong method takes the selected song
+     * and its index in the table view and sends its information to the
+     * EditSongController.
+     * @param event the event when the button is pressed
+     */
     @FXML
     private void editSong(ActionEvent event)
     {
@@ -330,6 +414,16 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * When the button labelled "delete" over at the tableview containing all
+     * songs is pressed then this method will run. This makes a popup window
+     * appear asking the user if they want to delete the selected song. If the
+     * user presses yes then the song will be deleted from the tabelview and
+     * the database. This is done by this method making calls to the method
+     * deleteSong from the SongModel and the method deleteSongFromTable from the
+     * PlaylistSongModel
+     * @param event the event when the button is pressed
+     */
     @FXML
     private void deleteSong(ActionEvent event)
     {
@@ -371,23 +465,23 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * The button labbelled "close" is pressed this close the program
+     * @param event the event when the button is pressed
+     */
     @FXML
     private void endApplication(ActionEvent event)
     {
         System.exit(0);
     }
 
-    private void searchSong(ActionEvent event)
-    {
-        try
-        {
-            tableSongList.setItems(sm.searchSongs(sm.getSongs(), writeSearch.getText().toLowerCase()));
-        } catch (IOException | MTBllException ex)
-        {
-            displayError(ex);
-        }
-    }
-
+    /**
+     * This method checks if the mediaplayer is currently playing a song. If it
+     * does it will skip to the song before the one currently playing. If the
+     * song is at index 0 in the list it will skip to the song at the last index
+     * @param event the event when the button right next to the stop button is
+     * pressed
+     */
     @FXML
     private void previousSong(ActionEvent event)
     {
@@ -420,6 +514,14 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * Checks if the mediaplayer is playing a song. If it does this method will
+     * skip to the song next to the one currently playing and play that song
+     * instead. If the song playing is at the last index in the list then
+     * it will skip to the song at index 0 aka the first song in the list
+     * @param event the event when the button next to the pause bause button is
+     * pressed
+     */
     @FXML
     private void nextSong(ActionEvent event)
     {
@@ -452,6 +554,20 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * This method checks the whether is mediaplayer is null, 
+     * is currently playing or is paused. If the mediaplayer is playing
+     * something and the play button is pressed then it will stop the song and
+     * play the currently selected song. If the mediaplayer is paused and 
+     * the play button is pressed then it will unpause and play the song from
+     * where it was. If the mediaplayer is null and a song is selected it will
+     * play that song
+     * If the mediaplayer is null and no song i selected a popup window will
+     * appear asking the use to select a song first
+     * When this method is called it will also call the controlSound method
+     * from this controller
+     * @param event th event when the play button is pressed
+     */
     @FXML
     private void playMusic(ActionEvent event)
     {
@@ -512,6 +628,11 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * When this method is called it will pause the currently playing song. If 
+     * a song isnt playing the method will do nothing
+     * @param event the event when the pause button is pressed
+     */
     @FXML
     private void pauseMusic(ActionEvent event)
     {
@@ -523,6 +644,12 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * When the stop button is pressed this methol will be called. Thie method
+     * stops the currently selected song and also clears the selection from
+     * both table views containing songs
+     * @param event 
+     */
     @FXML
     private void stopMusic(ActionEvent event)
     {
@@ -538,6 +665,17 @@ public class MyTunesViewController implements Initializable
         }
     }
 
+    /**
+     * This method calls the method searchSongs from the songModel class
+     * Everytime you press a key in the search textfield it will get the
+     * text from that field. The method searchSongs will then use that text
+     * as a parameter and also the list containing all songs. This will then set
+     * the items to the tableview to show songs that only contains the text from
+     * the textfield in their title or artist.
+     * @param event the event everytime a key on the keyboard is pressed on the
+     * textfield. So everytime a key is pressed on that field this method is
+     * called
+     */
     @FXML
     private void writeSearch(KeyEvent event)
     {
@@ -551,18 +689,38 @@ public class MyTunesViewController implements Initializable
 
     }
 
+    /**
+     * When the slider is dragged it gets its value on where its position is
+     * and uses that to set the song currently playing to a specific time
+     * @param event 
+     */
     @FXML
     private void sliderDrag(MouseEvent event)
     {
         mediaPlayer.seek(Duration.seconds(sliderDuration.getValue()));
     }
 
+    /**
+     * When a song is pressed it sets a boolean variable to false. This is so
+     * if you want to play a song while another song is playing it just plays
+     * the newly selected song instead of resuming the paused song. It also
+     * clears the selection off the tabelview that contains song on a specific
+     * playlist
+     * @param event everytime the tabelview containing all songs is clicked on
+     */
     private void songPressed(MouseEvent event)
     {
         paused = false;
         tableSongOnPlaylist.getSelectionModel().clearSelection();
     }
 
+    /**
+     * When the arrow button is pushed this method calls the addToPlaylist
+     * method from the PlaylistSongModel class. This will try to take a selected
+     * song and add it to the selected playlist
+     * @param event when the arrow button between the to tabelviews containing
+     * songs is pressed
+     */
     private void addToPlaylist(ActionEvent event)
     {
         try
@@ -576,19 +734,11 @@ public class MyTunesViewController implements Initializable
         }
     }
 
-    private void deleteFromPlaylistSongsEverything(ActionEvent event)
-    {
-    }
-
-        private void removeSongFromPlaylist(ActionEvent event)
-    {
-    }
-
-    private void endApplication()
-    {
-        System.exit(0);
-    }
-
+    
+    /**
+     * Opens a popup window displaying an exception
+     * @param ex the exception being displayed
+     */
     private void displayError(Exception ex)
     {
         Alert alert = new Alert(AlertType.INFORMATION);
